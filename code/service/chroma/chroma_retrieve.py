3004c21195d11c31cdf75d39e31c9e33e76b5bb0
AngKM <102318580+AngKM@users.noreply.github.com>
2026-03-31 12:03:54 +0700

    Added Embedding for retrieve

--- code/service/chroma/chroma_retrieve.py
+++ code/service/chroma/chroma_retrieve.py
@@ -1,13 +1,23 @@
 import chromadb
-from config import DATA_NAME
+from chromadb.utils import embedding_functions 
+from config import DATA_NAME, EMBEDDING_MODEL 
 
 class ChromaRetrieve:
     def __init__(self):
-        self.collection = chromadb.PersistentClient(path="./files/"+DATA_NAME).get_or_create_collection(name=DATA_NAME)
-        print("Retrieving data from:", "./files/"+DATA_NAME)
+        self.embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
+            model_name=EMBEDDING_MODEL
+        )
+        
+        self.client = chromadb.PersistentClient(path="./files/" + DATA_NAME)
+        
+        self.collection = self.client.get_or_create_collection(
+            name=DATA_NAME, 
+            embedding_function=self.embedding_function
+        )
+        print("Retrieving data from:", "./files/" + DATA_NAME)
     
     def retrieve(self, query):
         return self.collection.query(
             query_texts=[query],
-            n_results=100
+            n_results=10 
         )
