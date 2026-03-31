cc8b1b8d00484a8034d495346f64d1fdc29732f7
AngKM <102318580+AngKM@users.noreply.github.com>
2026-03-31 11:18:56 +0700

    Added Embedding Models Option

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
-DATA_NAME = "electronic_product"
+RAW_DATA_FOLDER_PATH = "./files/"
+DATA_NAME = "rai_curriculum"
+
+# Embedding Model
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
 # [OpenAI, Anthropic]
 MODEL_COMPANY = "OpenAI"
 MODEL_NAME = "gpt-4o"
-RAW_DATA_FOLDER_PATH = "./files/"
+
+# RAGAS Evaluation
+CRITIC_MODEL = "gpt-4o"
+GENERATOR_MODEL = "gpt-4o"
+
